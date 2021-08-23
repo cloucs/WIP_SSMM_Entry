6936069905c3f78f0b9bfd4b3a4034b0ac4821d0
unknown <ziemann.melvin@gmail.com>
2021-08-24 00:50:42 +0200

    added speed line particle system

--- Assets/Scripts/C_BossAnimation.cs
+++ Assets/Scripts/C_BossAnimation.cs
@@ -8,23 +8,19 @@ public class C_BossAnimation : MonoBehaviour
     public float speed = 5f;
     //adjust this to change how high it goes
     public float height = 0.5f;
-    public Transform gObj;
     // Start is called before the first frame update
+    private Vector3 newPos;
     void Start()
     {
-        Debug.Log("pos: " + gObj.transform.position);
-        Vector3 startPos = new Vector3(0, 15.0f, 26.0f);
-        gObj.position = startPos;
     }
  
     void Update() 
     {
         //get the objects current position and put it in a variable so we can access it later with less code
-        Debug.Log("pos: " + gObj.transform.position);
-        Vector3 pos = gObj.position;
+        Vector3 pos = this.transform.position;
         //calculate what the new Y position will be
         float newY = Mathf.Sin(Time.time * speed);
         //set the object's Y to the new calculated Y
-        gObj.position = new Vector3(pos.x, newY, pos.z) * height;
+        this.transform.position = new Vector3(pos.x, newY, pos.z) * height;
     }
 }
